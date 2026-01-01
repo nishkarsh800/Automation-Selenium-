aada4160716ec7fc865f0325500210ed7a41ebc5
NISHKARSH SAXENA <94742536+nishkarsh800@users.noreply.github.com>
2026-01-01 15:30:46 +0530

    feat: impl a fn for screenshots

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,15 +1,18 @@
 package base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class BaseTests {
@@ -30,6 +33,19 @@ public class BaseTests {
     @AfterClass
     public void tearDown(){
         driver.quit();
+    }
+
+    @AfterMethod
+    public void takeScreenShot(){
+        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            Files.move(
+                    screenshot.toPath(),
+                    Paths.get("resources/screenshots/test.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public WindowManager getWindowManager(){
